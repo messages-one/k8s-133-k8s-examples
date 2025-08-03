2dc1dd375d6057866286c2bd57f1603d5e3bb990
atlantis <atlantis@kubuntu.com>
2025-08-03 21:11:03 +0530

    commented the enum keyword

--- generate.sh
+++ generate.sh
@@ -9,25 +9,28 @@ for file in *.yaml; do
         echo -e "\nprocessing resource $filename\n\n" 
         
 	# remove all the lines starting from the word KIND and upto FIELDS
-        sed -i '/^KIND/,/^FIELDS/d' $filename.yaml
+        #sed -i '/^KIND/,/^FIELDS/d' $filename.yaml
 
-        sed -i -E 's/[[:space:]]+</\: \#</g' ${filename}.yaml
+        #sed -i -E 's/[[:space:]]+</\: \#</g' ${filename}.yaml
 	
 	# remove the first line from the file	
-        sed -i '1d' $filename.yaml
+        #sed -i '1d' $filename.yaml
+
+	# find the word enum and comment it with a #
+	sed -i.bak -E 's/(^[[:space:]]*)enum:/\1#enum:/g' $filename.yaml
 	
 	# sometimes we need to remove 3 spaces sometimes 2 spaces
         # data="$(cut -c 3- $filename.yaml)" 
 
         # remove 2 spaces at the beginning
-        data="$(sed 's/^  //' ${filename}.yaml)"
+        #data="$(sed 's/^  //' ${filename}.yaml)"
 
         # data="$(echo "$data" | yq -P '.')"
-        data="$(echo "$data")"
-        echo "$data"
-        rm -f ${filename}.yaml
-        echo "$data" | tee -a ${filename}.yaml 
-        echo '     ----      '
+        #data="$(echo "$data")"
+        #echo "$data"
+        #rm -f ${filename}.yaml
+        #echo "$data" | tee -a ${filename}.yaml 
+        #echo '     ----      '
     fi
 done
 
